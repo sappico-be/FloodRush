249631df0a76263ebdacbd367990006092768a5a
Kris Weytjens <kris.weytjens@sappico.be>
2025-09-14 11:23:34 +0200

    Initial Commit

---
+++ FloodRush/FloodRushApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloodRushApp.swift
+//  FloodRush
+//
+//  Created by Kris Weytjens on 14/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct FloodRushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
